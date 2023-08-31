98470e051b9d10495660fcf4ef758561875fd236
Amadeo Bon <amadeosph@outlook.com>
2023-08-30 22:56:58 -0300

    add api

--- eplay-main/src/components/ProductsList/index.tsx
+++ eplay-main/src/components/ProductsList/index.tsx
@@ -1,4 +1,4 @@
-import Game from '../../models/Game'
+import { Game } from '../../Pages/Home'
 import Product from '../Product'
 import { Container, List, Title } from './styles'
 
@@ -7,25 +7,51 @@ export type Props = {
   background: 'gray' | 'black'
   games: Game[]
 }
-const ProductsList = ({ background, title, games }: Props) => (
-  <Container background={background}>
-    <div className="container">
-      <Title>{title}</Title>
-      <List>
-        {games.map((game) => (
-          <Product
-            key={game.id}
-            title={game.title}
-            category={game.category}
-            system={game.system}
-            description={game.description}
-            infos={game.infos}
-            image={game.image}
-          />
-        ))}
-      </List>
-    </div>
-  </Container>
-)
+export const priceFormart = (preco = 0) => {
+  return new Intl.NumberFormat('pt-br', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(preco)
+}
+const ProductsList = ({ background, title, games }: Props) => {
+  const getGameTags = (game: Game) => {
+    const tags = []
+    if (game.release_date) {
+      tags.push(game.release_date)
+    }
+    if (game.prices.discount) {
+      tags.push(` ${game.prices.discount}%`)
+    }
+    if (game.prices.current) {
+      tags.push(priceFormart(game.prices.current))
+    }
+    if (game.prices.old) {
+      tags.push(priceFormart(game.prices.old))
+    }
+    return tags
+  }
+  return (
+    <Container background={background}>
+      <div className="container">
+        <Title>{title}</Title>
+        <List>
+          {games.map((game) => (
+           <li>
+             <Product
+              key={game.id}
+              title={game.name}
+              category={game.details.category}
+              system={game.details.system}
+              description={game.description}
+              infos={getGameTags(game)}
+              image={game.media.thumbnail}
+            />
+           </li>
+          ))}
+        </List>
+      </div>
+    </Container>
+  )
+}
 
 export default ProductsList
